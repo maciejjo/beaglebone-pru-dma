0e8663654c0edc5eea43a4e832f9181599a617b2
Maciej Sobkowski <maciej@sobkow.ski>
2017-06-28 14:18:03 +0200

    driver: Add stub for platform driver
    Add basic skeleton for platform driver, use module_platform_driver macro
    instead of init and exit functions
    
    Signed-off-by: Maciej Sobkowski <maciej@sobkow.ski>

--- kmod/pru_dma.c
+++ kmod/pru_dma.c
@@ -1,19 +1,57 @@
-#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/pruss.h>
+#include <linux/pinctrl/consumer.h>
 
-static int __init pru_dma_init(void)
+#define DRV_NAME "pru_dma"
+
+struct pru_dma_data {
+	struct device *dev;
+};
+
+int pru_dma_probe(struct platform_device *pdev)
 {
-	pr_debug("PRU DMA kmod\n");
+	struct pru_dma_data *pru_dma;
+	int ret;
+
+	pru_dma = devm_kzalloc(&pdev->dev, sizeof(*pru_dma), GFP_KERNEL);
+	if (!pru_dma)
+		return -ENOMEM;
+
+	pru_dma->dev = &pdev->dev;
+
+	dev_set_drvdata(&pdev->dev, pru_dma);
+
+	dev_dbg(pru_dma->dev, "Probe success");
+
 	return 0;
 }
 
-static void __exit pru_dma_exit(void)
+int pru_dma_remove(struct platform_device *pdev)
 {
+	return 0;
 }
 
-module_init(pru_dma_init);
-module_exit(pru_dma_exit);
 
-MODULE_LICENSE("GPL v2");
+static const struct of_device_id pru_dma_ids[] = {
+	{ .compatible = "pru-dma", },
+	{},
+};
+MODULE_DEVICE_TABLE(of, pru_dma_ids);
+
+static struct platform_driver pru_dma_driver = {
+	.driver = {
+		.name = DRV_NAME,
+		.owner = THIS_MODULE,
+		.of_match_table = pru_dma_ids,
+	},
+	.probe = pru_dma_probe,
+	.remove = pru_dma_remove,
+};
+
+module_platform_driver(pru_dma_driver);
+
+MODULE_DESCRIPTION("PRU DMA driver");
 MODULE_AUTHOR("Maciej Sobkowski");
-
+MODULE_LICENSE("GPL");
