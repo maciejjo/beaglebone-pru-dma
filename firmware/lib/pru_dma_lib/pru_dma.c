016882e3b78e3911dd139ec432ebab3225903fdb
Maciej Sobkowski <maciej@sobkow.ski>
2017-07-26 11:53:42 +0200

    firmware: Initialize rpmsg channel only once
    When using pru_dma_init more then once, only initialize rpmsg channel on
    first call.
    
    Signed-off-by: Maciej Sobkowski <maciej@sobkow.ski>

--- firmware/lib/pru_dma_lib/pru_dma.c
+++ firmware/lib/pru_dma_lib/pru_dma.c
@@ -17,6 +17,7 @@
 static volatile uint32_t *edma_ptr;
 static struct pru_rpmsg_transport transport;
 static uint16_t rpmsg_src, rpmsg_dst, rpmsg_len;
+uint8_t rpmsg_init_done = 0;
 
 struct pru_dma_tx_desc {
 	uint32_t kbuf_addr;
@@ -33,38 +34,55 @@ void pru_dma_init(struct pru_dma_data *dma_data,
 			struct fw_rsc_vdev_vring *rpmsg_vring0,
 			struct fw_rsc_vdev_vring *rpmsg_vring1)
 {
-	volatile uint8_t *status;
 
-	status = &rpmsg_vdev->status;
-	while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK));
+	if (!rpmsg_init_done) {
+		volatile uint8_t *status;
 
-	pru_rpmsg_init(&transport, rpmsg_vring0, rpmsg_vring1,
-			EVT_TO_ARM_HOST, EVT_FROM_ARM_HOST);
+		status = &rpmsg_vdev->status;
 
-	while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME,
-				CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
+		while (!(*status & VIRTIO_CONFIG_S_DRIVER_OK))
+			;
 
-	while (!(__R31 & HOST1_INT))
-		;
+		pru_rpmsg_init(&transport, rpmsg_vring0, rpmsg_vring1,
+				EVT_TO_ARM_HOST, EVT_FROM_ARM_HOST);
 
-	CT_INTC.SICR_bit.STS_CLR_IDX = EVT_FROM_ARM_HOST;
-	while (pru_rpmsg_receive(&transport, &rpmsg_src, &rpmsg_dst, &tx_desc, &rpmsg_len) == PRU_RPMSG_SUCCESS) 
-		;
+		while (pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME,
+					CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS)
+			;
 
-	edma_buf.src = tx_desc.kbuf_addr;
-	edma_buf.dst = PRU_SHMEM_OFFSET;
-	edma_buf.chan = tx_desc.edma_chan;
-	edma_buf.slot = tx_desc.edma_slot;
-	edma_buf.size = tx_desc.kbuf_size;
+		rpmsg_init_done = 1;
+	}
 
-	edma_ptr = EDMA0_CC_BASE;
+	while (1) {
 
-	/* Set up EDMA for transfer */
-	edma_setup(edma_ptr, &edma_buf);
+		if (__R31 & HOST1_INT) {
 
-	dma_data->src = edma_buf.src;
-	dma_data->dst = edma_buf.dst;
-	dma_data->size = edma_buf.size;
+			CT_INTC.SICR_bit.STS_CLR_IDX = EVT_FROM_ARM_HOST;
+
+			if (pru_rpmsg_receive(&transport, &rpmsg_src,
+						&rpmsg_dst, &tx_desc,
+						&rpmsg_len) ==
+					PRU_RPMSG_SUCCESS) {
+
+				edma_buf.src = tx_desc.kbuf_addr;
+				edma_buf.dst = PRU_SHMEM_OFFSET;
+				edma_buf.chan = tx_desc.edma_chan;
+				edma_buf.slot = tx_desc.edma_slot;
+				edma_buf.size = tx_desc.kbuf_size;
+
+				edma_ptr = EDMA0_CC_BASE;
+
+				/* Set up EDMA for transfer */
+				edma_setup(edma_ptr, &edma_buf);
+
+				dma_data->src = edma_buf.src;
+				dma_data->dst = edma_buf.dst;
+				dma_data->size = edma_buf.size;
+
+				break;
+			}
+		}
+	}
 }
 
 void pru_dma_trigger()
